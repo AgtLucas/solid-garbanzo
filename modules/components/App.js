4b7448e49da8f804c1da6d007da3634c8e81db7d
Lucas <lucas@agtlucas.com>
2016-03-15 06:53:03 -0300

    JS standard

--- modules/components/App.js
+++ modules/components/App.js
@@ -3,19 +3,18 @@ import { IndexLink, Link } from 'react-router'
 import Title from 'react-title-component'
 
 export default React.createClass({
-  render() {
+  render () {
     return (
       <div>
-        <Title render="Awesome App"/>
+        <Title render='Awesome App'/>
         <h1>Welcome to your app.</h1>
         <ul>
-          <li><IndexLink to="/">Home</IndexLink></li>
-          <li><Link to="/dragon">A DRAGON!</Link></li>
-          <li><Link to="/not-dragon">An old URL to a DRAGON!</Link></li>
+          <li><IndexLink to='/'>Home</IndexLink></li>
+        <li><Link to='/dragon'>A DRAGON!</Link></li>
+        <li><Link to='/not-dragon'>An old URL to a DRAGON!</Link></li>
         </ul>
         {this.props.children}
       </div>
     )
   }
 })
-
